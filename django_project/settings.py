ea08968c72ff2483d5315b27ce11011f53337972
Melaku <>
2023-07-04 12:26:23 -0400

    updated settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x)+*wdlljg%1nq+5=fo#wkpoe*n@u9+z-5f6q%b3v4pwb7-xf4'
-
+# SECRET_KEY = 'django-insecure-x)+*wdlljg%1nq+5=fo#wkpoe*n@u9+z-5f6q%b3v4pwb7-xf4'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['atmelaku-13fea32952f5.herokuapp.com']
 
 
 # Application definition
@@ -133,6 +133,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'alebachewmelaku58@gmail.com'
-EMAIL_HOST_PASSWORD = 'pudrcamclziuwzsc'
+EMAIL_HOST_USER='alebachewmelaku58@gmail.com'
+EMAIL_HOST_PASSWORD='pudrcamclziuwzsc'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
