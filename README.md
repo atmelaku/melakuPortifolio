d64f5a211d89bcf29efca61d961ae652f789f583
Melaku <>
2023-07-04 11:45:59 -0400

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # django_project
+[ANSWERED]-DJANGO ADMIN AND SQLITE: DATABASE DISK IMAGE IS MALFORMED-DJANGO
+Only three steps.
+
+delete db.sqlite3 and all migrations files
+run python manage.py makemigrations
+run python manage.py migrate
+source:
+https://ts-cset.github.io/cset-160/week-05/virtual-environments.html#10
+
+https://www.cs.sfu.ca/~ashriram/Courses/CS295/assets/books/CSAPP_2016.pdf
---
+++ blog/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.CharField(max_length=1000)
+    date_posted = models.DateTimeField(default=timezone.now)
+    # telling django to delete if the user is null del post
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+        # this is for create post
+    def get_absolute_url(self):
+        return reverse("post-detail", kwargs={"pk": self.pk})
